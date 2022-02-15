5c8a6a1b20ce0acfa40b3ca5aa8fd71456626399
Bonganibg <bongani.gumbo@yahoo.com>
2022-02-15 07:08:14 +0200

    mock up project information page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ProjectInfoPageComponent } from './project-info-page/project-info-page.component';
+import { MarkdownModule } from 'ngx-markdown';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    NavBarComponent
+    NavBarComponent,
+    ProjectInfoPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MarkdownModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({loader: HttpClient})
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/project-info-page/project-info-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-info-page',
+  templateUrl: './project-info-page.component.html',
+  styleUrls: ['./project-info-page.component.scss']
+})
+export class ProjectInfoPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
