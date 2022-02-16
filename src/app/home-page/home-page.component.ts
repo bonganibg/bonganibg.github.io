2c671f0da6e6961b627d977495a9a9d5a4e73549
Bonganibg <bongani.gumbo@yahoo.com>
2022-02-16 11:46:08 +0200

    skill card format updated, parallax added to header

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,3 +1,4 @@
+import { style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    window.addEventListener('scroll', function(e){
+      // Header scrolling
+      const logo = document.querySelector('.scroll') as HTMLSelectElement;
+      const circle = this.document.querySelector('.hero-background-item') as HTMLSelectElement;
+
+      var scrolled = window.pageYOffset;
+      var rate = scrolled * 0.1;
+      logo.style.transform = `translate3d(${rate}px, 0px, 0px)`;
+
+      var circleRate = scrolled * 0.15;
+      circle.style.transform = `translate3d(1px, ${circleRate}px, 0px)`;
+
+
+
+      // Skills letter Scrolling
+      const left = document.querySelector('.scroll-left') as HTMLScriptElement;
+      const right = document.querySelector('.scroll-right') as HTMLScriptElement;
+
+      var textRate = scrolled * 0.5;
+      left.style.transform = `translate3d(${textRate}px, 0px, 0px)`;
+      right.style.transform = `translate3d(-${textRate}px, 0px, 0px)`;
+
+
+    })
+  }
 
   ngOnInit(): void {
   }
+
+
 
 }
