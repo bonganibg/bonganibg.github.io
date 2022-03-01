ae235112cda2579717158fbdd39a863dc97209c0
Bonganibg <bongani.gumbo@yahoo.com>
2022-03-01 08:48:48 +0200

    Featured project page updated

--- src/app/featured-projects/featured-projects.component.ts
+++ src/app/featured-projects/featured-projects.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-featured-projects',
   templateUrl: './featured-projects.component.html',
-  styleUrls: ['./featured-projects.component.css']
+  styleUrls: ['./featured-projects.component.scss']
 })
 export class FeaturedProjectsComponent implements OnInit {
 
   constructor() { }
+
+  projects = [
+    "MiBo Maps",
+    "Chicken Hop Skip and Jump",
+    "Bluebottle Sting"
+  ]
 
   ngOnInit(): void {
   }
