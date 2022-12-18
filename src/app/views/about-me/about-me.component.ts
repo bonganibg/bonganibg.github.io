7f44df881aac2d4bdad606ee5e6670e4b7751ace
Bonganibg <bongani.gumbo@yahoo.com>
2022-12-18 15:40:42 +0200

    about me page navigation done

--- src/app/views/about-me/about-me.component.ts
+++ src/app/views/about-me/about-me.component.ts
@@ -9,7 +9,18 @@ export class AboutMeComponent implements OnInit {
 
   constructor() { }
 
+  currentPage = 0;
+
   ngOnInit(): void {
+  }
+
+  changePage(page: number){
+    if (page > -1 && page < 4)
+    this.currentPage = page;
+  }
+
+  getClass(page: number){
+    return page == this.currentPage ? "tab px-0" : "tab px-0 tab-active";
   }
 
 }
