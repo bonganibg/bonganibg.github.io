9dcdef3a11341bd834603790156872e017dd9d10
Bonganibg <bongani.gumbo@yahoo.com>
2023-01-17 14:02:52 +0200

    first build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  LINK_GITHUB: "https://www.github.com/bonganibg",
+  LINK_LINKEDIN: "https://www.linkedin.com/in/bonganibg",
+  LINK_TWITTER: "https://www.twitter.com/bonganibg",
+  PAGE_DATA_LANDING_PAGE: "/assets/content/landing-page.json",
+  API_BASE_URL: "https://bonganibg-api.onrender.com",
+  API_QUESTION_ENDPOINT: "/about/question",
+  API_ABOUT_ME_ENDPOINT: "/about/me",
+  API_TECH_ENDPOINT: "/about/tech",
+  API_TECH_DETAILS_ENDPOINT: "/about/tech/detail",
+  API_PROJECT_ENDPOINT: "/project",
+  API_PROJECT_TAG_ENDPOINT: "/project/tag"
 };
